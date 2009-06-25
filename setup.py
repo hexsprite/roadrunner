761dfb4bc9033387a0a78977583163d2ca6fddc7
Matthew Wilkes <matthewwilkes@Nimbus.internal.teamrubber.com>
2009-06-25 17:48:29 +0100

    Bumped version number on the 4th level, as this only fixes packaging, no features

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2.3'
+version = '0.2.3.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
